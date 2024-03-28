d6340e7c69fc88e64682c46c194502d112d0bfe8
Amardeep Kahlon <asingh@innow8apps.com>
2024-03-28 11:32:16 +0530

    Update like.py
    error handling improvement

--- routers/like.py
+++ routers/like.py
@@ -13,6 +13,15 @@ def like_post(post_id: int, db: Session = Depends(get_db), current_user: models.
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+    if (
+        existing_like := db.query(models.Like)
+        .filter(
+            models.Like.user_id == current_user.id,
+            models.Like.post_id == post_id,
+        )
+        .first()
+    ):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You have already liked this post")
     like = models.Like(user_id=current_user.id, post_id=post_id)
     db.add(like)
     db.commit()
